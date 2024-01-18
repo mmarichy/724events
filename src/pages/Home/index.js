26f8474777eb969590b7e222160c3786c585644e
mathis marichy <marichy.pro@gmail.com>
2024-01-18 15:41:52 +0100

    Fix image and label Footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,8 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const {data} = useData()
+  const last = data?.events[data.events.length - 1];
   return <>
     <header>
       <Menu />
@@ -116,13 +117,15 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
+        {last &&(
         <EventCard
           imageSrc={last?.cover}
           title={last?.title}
           date={new Date(last?.date)}
           small
-          label="boom"
+          label={last.type}
         />
+        )}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
