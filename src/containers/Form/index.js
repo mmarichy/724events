a3254d6ec0547202a79618cff4ee24d7229dd6d2
mathis marichy <marichy.pro@gmail.com>
2024-01-18 15:39:22 +0100

    Fix Form modal display | Test message sent

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,8 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        // Ajout de la fonction onSuccess pour afficher la modal.
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
